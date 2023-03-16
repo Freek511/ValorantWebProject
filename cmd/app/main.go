edcec74d460f86c247c461e9863b544eb30f3c9c
glamostoffer <glamostoffer@gmail.com>
2023-03-16 13:12:09 +0300

    Worked on project structure

---
+++ cmd/app/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Using a main page")
+	tem, err := template.ParseFiles("resources/index.html")
+	if err != nil {
+		log.Println(err)
+	}
+	tem.Execute(w, nil)
+}
+
+func agents(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Using an agents page")
+  tem, err := template.ParseFiles("resources/agents.html")
+  if err != nil {
+    log.Println(err)
+  }
+  tem.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Using a login page")
+	tem, err := template.ParseFiles("resources/login.html")
+	if err != nil {
+		log.Println(err)
+	}
+	tem.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/style/", http.StripPrefix("/style", http.FileServer(http.Dir("./resources/static"))))
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/login", login)
+  http.HandleFunc("/agents", agents)
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8080", nil)
+}
