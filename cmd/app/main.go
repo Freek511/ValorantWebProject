1f82259beaff15a40c193ae8f14115aa8288083d
glamostoffer <glamostoffer@gmail.com>
2023-03-16 13:30:01 +0300

    Navbar is working now

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -17,12 +17,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func agents(w http.ResponseWriter, r *http.Request) {
-  fmt.Println("Using an agents page")
-  tem, err := template.ParseFiles("resources/agents.html")
-  if err != nil {
-    log.Println(err)
-  }
-  tem.Execute(w, nil)
+	fmt.Println("Using an agents page")
+	tem, err := template.ParseFiles("resources/agents.html")
+	if err != nil {
+		log.Println(err)
+	}
+	tem.Execute(w, nil)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -34,11 +34,21 @@ func login(w http.ResponseWriter, r *http.Request) {
 	tem.Execute(w, nil)
 }
 
+func support(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Using a support page")
+	tem, err := template.ParseFiles("resources/support.html")
+	if err != nil {
+		log.Println(err)
+	}
+	tem.Execute(w, nil)
+}
+
 func main() {
 	http.Handle("/style/", http.StripPrefix("/style", http.FileServer(http.Dir("./resources/static"))))
 	http.HandleFunc("/home", home)
 	http.HandleFunc("/login", login)
-  http.HandleFunc("/agents", agents)
+	http.HandleFunc("/agents", agents)
+	http.HandleFunc("/support", support)
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":8080", nil)
 }
