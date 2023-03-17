525be31055635631b6e8e251a80f45cb198e680e
glamostoffer <glamostoffer@gmail.com>
2023-03-17 23:33:03 +0300

    Added go mod and cleared main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
+	"ValorantWebProject/internal/controllers"
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Using a main page")
-	tem, err := template.ParseFiles("resources/index.html")
-	if err != nil {
-		log.Println(err)
-	}
-	tem.Execute(w, nil)
-}
-
-func agents(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Using an agents page")
-	tem, err := template.ParseFiles("resources/agents.html")
-	if err != nil {
-		log.Println(err)
-	}
-	tem.Execute(w, nil)
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Using a login page")
-	tem, err := template.ParseFiles("resources/login.html")
-	if err != nil {
-		log.Println(err)
-	}
-	tem.Execute(w, nil)
-}
-
-func support(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Using a support page")
-	tem, err := template.ParseFiles("resources/support.html")
-	if err != nil {
-		log.Println(err)
-	}
-	tem.Execute(w, nil)
-}
-
 func main() {
 	http.Handle("/style/", http.StripPrefix("/style", http.FileServer(http.Dir("./resources/static"))))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/agents", agents)
-	http.HandleFunc("/support", support)
+	http.HandleFunc("/", controllers.Home)
+	http.HandleFunc("/login", controllers.Login)
+	http.HandleFunc("/agents", controllers.Agents)
+	http.HandleFunc("/support", controllers.Support)
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":8080", nil)
 }
